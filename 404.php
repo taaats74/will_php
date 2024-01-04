fe7fa441a9ea8447c0661ed5780936b01809fa74
taaats74 <taaats74@gmail.com>
2024-01-04 15:27:39 +0900

    done

---
+++ 404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+  <section class="page-not-found">
+    <div class="container">
+      <div class="wrapper">
+        <h2>404</h2>
+        <h3>Not Found</h3>
+        <p class="p1">アクセスしようとしたページは見つかりませんでした。</p>
+        <div class="box-wrapper">
+          <p>以下のような原因が考えられます。</p>
+          <ul>
+            <li>・アクセスしようとしたページが存在しない。</li>
+            <li>・URLが間違っている。</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+<?php get_footer(); ?>
