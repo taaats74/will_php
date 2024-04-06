6ebaf65fe140dfc7b8f27022839a41a453be8d95
taaats74 <taaats74@gmail.com>
2024-04-07 05:28:07 +0900

    working on price section

--- page-price.php
+++ page-price.php
@@ -85,7 +85,7 @@
                   </ul>
                 </div>
                 <div class="price">
-                  <p><span>50</span>万円〜</p>
+                  <p><span>50</span>万円〜(税別)</p>
                 </div>
               </div>
               <div class="box">
@@ -122,7 +122,7 @@
                   </ul>
                 </div>
                 <div class="price">
-                  <p><span>120</span>万円〜</p>
+                  <p><span>120</span>万円〜(税別)</p>
                 </div>
               </div>
               <div class="box">
@@ -159,13 +159,13 @@
                   </ul>
                 </div>
                 <div class="price">
-                  <p><span>220</span>万円〜</p>
+                  <p><span>220</span>万円〜(税別)</p>
                 </div>
               </div>
             </div>
           </div>
           <div class="service double">
-            <h2 class="price-header"><span>W</span>ebマーケティング支援<span class="pc">・</span><br class="sp"><span>W</span>eb広告運用代行</h2>
+            <h2 class="price-header"><span>W</span>ebマーケティング支援</h2>
             <div class="border"></div>
             <p class="ex">費用例</p>
             <div class="box-wrapper web">
@@ -175,27 +175,14 @@
                     <p>01</p>
                   </div>
                   <div class="title">
-                    <h3 class="plan">Webマーケティング</h3>
+                    <h3 class="plan">集客サポートプラン</h3>
                   </div>
                 </div>
                 <div class="desc">
-                  <ul>
-                    <li>
-                      <p class="li-desc">・マーケティング戦略設計</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・アクセス解析 / 分析</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・Webサイトの最適化</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・Web施策の各種ご提案</p>
-                    </li>
-                  </ul>
+                  <p class="intro">zoomで月1回/1時間でサービスや商品の集客に関するご相談から、SNS、MEO、ブログ運用などのご相談をお受けいたします。</p>
                 </div>
                 <div class="price">
-                  <p><span>20</span>万円〜 / 月</p>
+                  <p><span>9,800</span>円〜 / 月</p>
                 </div>
               </div>
               <div class="box">
@@ -204,27 +191,14 @@
                     <p>02</p>
                   </div>
                   <div class="title">
-                    <h3 class="plan">Web広告</h3>
+                    <h3 class="plan">集客設計プラン</h3>
                   </div>
                 </div>
                 <div class="desc">
-                  <ul>
-                    <li>
-                      <p class="li-desc">・マーケティング戦略設計</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・リスティング広告</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・ディスプレイ広告</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・SNS広告</p>
-                    </li>
-                  </ul>
+                  <p class="intro">zoomで1回/2時間でマーケティングの差別化戦略や集客の動線設計を一緒に構築します。マーケティングの考え方なども合わせてお伝えするプランです。</p>
                 </div>
-                <div class="price text">
-                  <p>広告月額費の20%</p>
+                <div class="price">
+                  <p><span>30,000</span>円〜 / 回</p>
                 </div>
               </div>
             </div>
@@ -240,41 +214,10 @@
                     <p>01</p>
                   </div>
                   <div class="title">
-                    <h3 class="plan">内製化プラン</h3>
+                    <h3 class="plan">シンプルプラン</h3>
                   </div>
                 </div>
                 <p class="recommendation">社内での内製化を目指す企業様向け</p>
-                <div class="desc">
-                  <ul>
-                    <li>
-                      <p class="li-desc">・ノウハウ研修</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・分析レポート</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・数値検証</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・定例zoom</p>
-                    </li>
-                    <div class="li-desc etc">etc.</div>
-                  </ul>
-                </div>
-                <div class="price">
-                  <p><span>15</span>万円〜/月(税別)</p>
-                </div>
-              </div>
-              <div class="box">
-                <div class="title-wrapper">
-                  <div class="num">
-                    <p>02</p>
-                  </div>
-                  <div class="title">
-                    <h3 class="plan">初期構築プラン</h3>
-                  </div>
-                </div>
-                <p class="recommendation">一番大事な土台作りをサポート</p>
                 <div class="desc">
                   <ul>
                     <li>
@@ -284,15 +227,42 @@
                       <p class="li-desc">・競合リサーチ</p>
                     </li>
                     <li>
-                      <p class="li-desc">・デザイン作成</p>
+                      <p class="li-desc">・投稿ネタだし</p>
                     </li>
                     <li>
-                      <p class="li-desc">・運用ノウハウ研修</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・投稿のネタだし</p>
+                      <p class="li-desc">・ノウハウ講習</p>
                     </li>
                     <div class="li-desc etc">etc.</div>
+                  </ul>
+                </div>
+                <div class="price">
+                  <p><span>15</span>万円〜(税別)</p>
+                </div>
+              </div>
+              <div class="box">
+                <div class="title-wrapper">
+                  <div class="num">
+                    <p>02</p>
+                  </div>
+                  <div class="title">
+                    <h3 class="plan">スタンダードプラン</h3>
+                  </div>
+                </div>
+                <p class="recommendation">一番大事な土台作りをサポート</p>
+                <div class="desc">
+                  <ul>
+                    <li>
+                      <p class="li-desc">・シンプルプランの内容</p>
+                    </li>
+                    <li>
+                      <p class="li-desc">・デザインテンプレ作成</p>
+                    </li>
+                    <li>
+                      <p class="li-desc">・ハイライトテンプレ作成</p>
+                    </li>
+                    <li>
+                      <p class="li-desc">・1投稿作成</p>
+                    </li>
                   </ul>
                 </div>
                 <div class="price">
@@ -305,29 +275,22 @@
                     <p>03</p>
                   </div>
                   <div class="title">
-                    <h3 class="plan">運用代行プラン</h3>
+                    <h3 class="plan">プレミアムプラン</h3>
                   </div>
                 </div>
-                <p class="recommendation">運用をまるっと任せたい企業様向け</p>
+                <p class="recommendation">運用に早くのせたい企業様向け</p>
                 <div class="desc">
                   <ul>
                     <li>
-                      <p class="li-desc">・投稿のネタだし</p>
+                      <p class="li-desc">・スタンダードプランの内容</p>
                     </li>
                     <li>
-                      <p class="li-desc">・投稿作成</p>
+                      <p class="li-desc">・12投稿作成</p>
                     </li>
-                    <li>
-                      <p class="li-desc">・定例zoom</p>
-                    </li>
-                    <li>
-                      <p class="li-desc">・報告レポート</p>
-                    </li>
-                    <div class="li-desc etc">etc.</div>
                   </ul>
                 </div>
                 <div class="price">
-                  <p><span>40</span>万円〜/月(税別)</p>
+                  <p><span>30</span>万円〜(税別)</p>
                 </div>
               </div>
             </div>
