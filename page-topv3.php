6a39f34516eb3be2e25f62e29baff4082369f37b
taaats74 <taaats74@gmail.com>
2026-08-28 18:15:13 +0900

    style(topv3): ロゴ背景をさらに300px上げ、パララックスを下方向に反転
    - bottom を 200px から 500px に変更(さらに300px持ち上げ)
    - data-rellax-desktop-speed を 2 から -2 に変更。Rellax は速度の符号で
      向きが決まるため、負にするとスクロールに対して下方向へ動く。
      移動量は 100×|speed| px のままで上下±100px
    
    PCのみの変更で、SP(@media max-width:820px)は bottom:0 / speed:1 のまま。
    style.css は再コンパイルせず scss と該当行を手で対応させている。

--- page-topv3.php
+++ page-topv3.php
@@ -126,8 +126,8 @@
           </div>
         </div>
         <img src="<?php echo get_template_directory_uri(); ?>/img/page-top2-management-issue.webp" alt="management issue" class="text-image rellax-up">
-        <?php /* PC(821px以上)のみパララックスを大きくする。移動量は 100×speed px(speed:2 で上下±100px) */ ?>
-        <img src="<?php echo get_template_directory_uri(); ?>/img/page-top2-logo-bg.webp" alt="合同会社ウィル" class="logo-image rellax-up" data-rellax-desktop-speed="2">
+        <?php /* PC(821px以上)のみパララックスを上書き。速度が負なのでスクロールに対して下方向に動く(移動量は 100×speed px = 上下±100px) */ ?>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/page-top2-logo-bg.webp" alt="合同会社ウィル" class="logo-image rellax-up" data-rellax-desktop-speed="-2">
       </div>
     </section>
 
